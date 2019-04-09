a5e19f2ec3d98518de372144d93c01a76ba1ddc0
Alfred Aquino <alfredaquino@hotmail.com>
2019-04-09 11:04:12 -0400

    websrv.py
    websrv.py runs a tiny localhost web server on port 8080 (default) and only serves a single file: index.html (currently)

---
+++ websrv.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+from _thread import *
+import threading
+import http.server
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+DEFAULT_IP = '127.0.0.1'
+DEFAULT_PORT = 8080
+DEFAULT_EXIT = '/shutdown'
+DEFAULT_FILE = './index.html'
+
+class MyHandler(BaseHTTPRequestHandler):
+    def send_ok_headers(self):
+        self.send_response(200)                        
+        self.send_header('Content-type','text-html')  
+        self.end_headers() 
+        pass 
+
+    def send_bye_message(self):
+        self.wfile.write(bytes('bye','UTF-8'))
+        pass
+
+
+    def do_GET(self):
+        if self.path == DEFAULT_EXIT:
+            MyHandler.send_ok_headers(self)
+            MyHandler.send_bye_message(self)
+            self.server.running = False
+        else:
+            try:                
+                f = open(DEFAULT_FILE, 'rb') #open requested file  
+                MyHandler.send_ok_headers(self)
+                self.wfile.write(bytes(f.read()))
+                f.close()  
+                return  
+
+            except FileNotFoundError:  
+                self.send_error(404, 'file not found')
+
+class MainServer:
+    def __init__(self, port = DEFAULT_PORT):
+        self._server = HTTPServer((DEFAULT_IP, DEFAULT_PORT), MyHandler)
+        self._thread = threading.Thread(target=self.run)
+        self._thread.deamon = True 
+
+    def run(self):
+        self._server.running = True
+        while self._server.running:
+            self._server.handle_request()
+
+    def start(self):
+        self._thread.start()
+        print("host running")
+
+    def shut_down(self):
+        self._thread.close()
+        print("host shut down")
+
+m = MainServer()
+m.start()
+
+
